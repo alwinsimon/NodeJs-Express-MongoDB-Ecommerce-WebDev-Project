b04b25a415790e4199de76bb9116fc89504df149
Alwin Simon <003alwin@gmail.com>
2023-05-04 19:21:02 +0530

    Added new folder for Admin in VIEWS & configured products table, Modified Admin and User Partials,Added id to products Array in Admin and user.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,41 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  let products = [
+
+    {
+      _id:1,
+      name:"I Phone 11",
+      category:"mobile",
+      description:"Coolest Phone",
+      image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+    },
+    {
+      _id:2,
+      name:"MacBook Pro",
+      category:"laptop",
+      description:"Coolest Laptop Ever",
+      image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+    },
+    {
+      _id:3,
+      name:"Google Pixel",
+      category:"mobile",
+      description:"Best Android Phone Ever",
+      image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+    },
+    {
+      _id:4,
+      name:"Hp Laserjet",
+      category:"printer",
+      description:"Fastest Printer on the planet",
+      image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+    }
+
+  ]
+
+  res.render('admin/view-products',{title:"Admin Panel",admin:true,products});
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,24 +7,28 @@ router.get('/', function(req, res, next) {
   let products = [
 
     {
+      _id:1,
       name:"I Phone 11",
       category:"mobile",
       description:"Coolest Phone",
       image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
     },
     {
+      _id:2,
       name:"MacBook Pro",
       category:"laptop",
       description:"Coolest Laptop Ever",
       image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
     },
     {
+      _id:3,
       name:"Google Pixel",
       category:"mobile",
       description:"Best Android Phone Ever",
       image:"https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
     },
     {
+      _id:4,
       name:"Hp Laserjet",
       category:"printer",
       description:"Fastest Printer on the planet",
@@ -33,7 +37,7 @@ router.get('/', function(req, res, next) {
 
   ]
 
-  res.render('index', { title: 'Shopping Cart', products, admin:true });
+  res.render('index', { title: 'Shopping Cart', products, admin:false });
 
 });
 
