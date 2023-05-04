aaa1eed60c29276c8ef117711f08e7d2804785f8
Alwin Simon <003alwin@gmail.com>
2023-05-04 14:45:59 +0530

    Installed Express handle bars wil degraded version, app.engine configured in app.js, layouts and partials configured in views folder - WIP

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir: __dirname+'views/layout/',partialsDir: __dirname+'views/partials/',}))
+app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir: __dirname+'/views/layout/',partialsDir: __dirname+'/views/partials/'}));
 
 app.use(logger('dev'));
 app.use(express.json());
