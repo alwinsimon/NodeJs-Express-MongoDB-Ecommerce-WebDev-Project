2e0b83c70d502056069ea09872cc26ee6c6861fd
Alwin Simon <003alwin@gmail.com>
2023-07-08 18:14:40 +0530

    Added Try-Catch in product helpers

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -10,18 +10,28 @@ module.exports = {
 
     addProduct:(product,callback)=>{
 
-        db.get().collection(collections.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
+        try{
 
-            callback(data);
-            /*In the above line, we are passing the object named data.
-            This object  is obtained as a result of calling .then in the promise returned by the insertOne function of MongoDb.
-            This object has a key namely insertedId this is the insert id of document created in the db.
-            We send it back as an argument of the callback in admin.js where this function was called.
-            There this id is utilised for setting the name of uploaded image so that,
-            we can establish a relation between each product document in db and its image in server
-            */
-            
-        })
+            db.get().collection(collections.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
+
+                callback(data);
+                /*In the above line, we are passing the object named data.
+                This object  is obtained as a result of calling .then in the promise returned by the insertOne function of MongoDb.
+                This object has a key namely insertedId this is the insert id of document created in the db.
+                We send it back as an argument of the callback in admin.js where this function was called.
+                There this id is utilised for setting the name of uploaded image so that,
+                we can establish a relation between each product document in db and its image in server
+                */
+                
+            })
+
+        }catch(error){
+        
+            console.error("Error from addProduct product-helpers: ", error);
+        
+            reject(error);
+        
+        }
 
     },
     getAllProducts: () => {
@@ -72,7 +82,7 @@ module.exports = {
 
             } catch (error) {
 
-                console.log(error);
+                console.error("Error from getAllProducts product-helpers: ", error);
 
                 reject(error);
 
@@ -85,27 +95,37 @@ module.exports = {
 
         return new Promise((resolve, reject) => {
 
-            //Function to delete the document from MongoDb collection
-            db.get().collection(collections.PRODUCT_COLLECTION).deleteOne({ _id: ObjectId(productId) }).then((deleteResult) => {
+            try{
 
-                // Defining the path of the product image to be deleted
-                const imageName = image.concat('.jpg')
-                const imagePath = path.join(__dirname, '..', 'public', 'product-images', imageName);
+                //Function to delete the document from MongoDb collection
+                db.get().collection(collections.PRODUCT_COLLECTION).deleteOne({ _id: ObjectId(productId) }).then((deleteResult) => {
 
-                // Function to Delete the image file from the server using the above defined path
-                fs.unlink(imagePath, (err) => {
+                    // Defining the path of the product image to be deleted
+                    const imageName = image.concat('.jpg')
+                    const imagePath = path.join(__dirname, '..', 'public', 'product-images', imageName);
 
-                    if (err) {
-                        console.error(`Error deleting file ${imagePath}: ${err}`);
-                    }
-                    
+                    // Function to Delete the image file from the server using the above defined path
+                    fs.unlink(imagePath, (err) => {
+
+                        if (err) {
+                            console.error(`Error deleting file ${imagePath}: ${err}`);
+                        }
+                        
+                    });
+
+                    // console.log(deleteResult);
+
+                    resolve();
+
                 });
 
-                // console.log(deleteResult);
-
-                resolve();
-
-            });
+            }catch(error){
+            
+                console.error("Error from deleteProduct product-helpers: ", error);
+            
+                reject(error);
+            
+            }
 
         });
 
@@ -114,11 +134,21 @@ module.exports = {
 
         return new Promise((resolve,reject)=>{
 
-            db.get().collection(collections.PRODUCT_COLLECTION).findOne({_id:ObjectId(productId)}).then((product)=>{
+            try{
 
-                resolve(product);
+                db.get().collection(collections.PRODUCT_COLLECTION).findOne({_id:ObjectId(productId)}).then((product)=>{
 
-            })
+                    resolve(product);
+    
+                })
+            
+            }catch(error){
+            
+                console.error("Error from getProductDetails product-helpers: ", error);
+            
+                reject(error);
+            
+            }
 
         })
 
@@ -127,21 +157,31 @@ module.exports = {
 
         return new Promise((resolve,reject)=>{
 
-            db.get().collection(collections.PRODUCT_COLLECTION)
-            .updateOne({_id:ObjectId(productId)},{
-                $set:{
-                    id:productDetails.id,
-                    name:productDetails.name,
-                    category:productDetails.category,
-                    description:productDetails.description,
-                    price:productDetails.price
+            try{
+
+                db.get().collection(collections.PRODUCT_COLLECTION)
+                .updateOne({_id:ObjectId(productId)},{
+                    $set:{
+                        id:productDetails.id,
+                        name:productDetails.name,
+                        category:productDetails.category,
+                        description:productDetails.description,
+                        price:productDetails.price
+                    }
                 }
+                ).then(()=>{
+    
+                    resolve();
+    
+                })
+
+            }catch(error){
+            
+                console.error("Error from updateProduct product-helpers: ", error);
+            
+                reject(error);
+            
             }
-            ).then(()=>{
-
-                resolve();
-
-            })
 
         })
 
@@ -178,6 +218,8 @@ module.exports = {
                         })
                         .catch((error) => {
 
+                            console.error("Error-1 from db promise at getProductCategoryById product-helpers: ", error);
+
                             reject(error);
 
                         });
@@ -191,11 +233,15 @@ module.exports = {
                 })
                 .catch((error) => {
 
+                    console.error("Error-2 from db promise at getProductCategoryById product-helpers: ", error);
+
                     reject(error);
 
                 });
 
             } catch (error) {
+
+                console.error("Error from getProductCategoryById product-helpers: ", error);
 
                 reject(error);
 
@@ -204,9 +250,5 @@ module.exports = {
         });
 
     }
-      
-      
-      
-      
 
 }
