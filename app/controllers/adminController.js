924e72ecedd05885dc742ba552b240fccecbc880
Dr. Alwin Simon <003alwin@gmail.com>
2024-07-23 20:49:13 +0530

    Minor fix in health route response.

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -26,7 +26,7 @@ const healthStatusGET = (req, res) => {
   const formattedDate = currentDate.toLocaleString("en-US", options);
 
   res.status(200).json({
-    status: `${process.env.APPLICATION_NAME} and Systems are Up & Running.`,
+    status: `${process.env.PLATFORM_NAME} Server and Systems are Up & Running.`,
     dateTime: formattedDate,
   });
 
